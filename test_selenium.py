7210240d7089361742e05e8701246652ac24c231
Karol Kolanski <gralu86@gmail.com>
2022-02-05 14:37:24 +0100

    Steps..

--- test_selenium.py
+++ test_selenium.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 import unittest
 
@@ -8,15 +9,22 @@ class TestRegistration(unittest.TestCase):
         self.driver = webdriver.Firefox()
         self.driver.get("https://www.eobuwie.com.pl/")
         self.driver.maximize_window()
+        accept_button = self.driver.find_element(By.XPATH, '//button[@data-testid="permission-popup-accept"]')
+        accept_button.click()
 
     def tearDown(self):
-        # Po teście wyłączamy przeglądarke
+        # Po teście wyłączamy przeglądarkę
         self.driver.quit()
 
     def testinvalidEmail(self):
         # KROKI
         # 1. Kliknij Zarejestruj - otwiera się strona rejestracji
-        pass
+        driver = self.driver
+        register_btn = driver.find_element(By.PARTIAL_LINK_TEXT, "Zarejestruj")
+        register_btn.click()
+        # Poczekaj chwilę, żeby zobaczyć co się dzieje
+        sleep(4)
+
 
 
 if __name__ =="__main__":
