62816a078f8bdc2264c189f59782a4cf5074c01b
Karol Kolanski <gralu86@gmail.com>
2022-02-05 13:58:49 +0100

    requirements added

--- test_selenium.py
+++ test_selenium.py
@@ -4,8 +4,20 @@ import unittest
 
 class TestRegistration(unittest.TestCase):
     def setUp(self):
-        pass
+        # Przed testwem otwieramy stronę
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://www.eobuwie.com.pl/")
+        self.driver.maximize_window()
+
     def tearDown(self):
-        pass
+        # Po teście wyłączamy przeglądarke
+        self.driver.quit()
+
     def testinvalidEmail(self):
+        # KROKI
+        # 1. Kliknij Zarejestruj - otwiera się strona rejestracji
         pass
+
+
+if __name__ =="__main__":
+    unittest.main()
