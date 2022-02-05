a153924dc364698e564182f0b8b2bf0703a1730f
Karol Kolanski <gralu86@gmail.com>
2022-02-05 13:50:41 +0100

    Selenium test

---
+++ test_selenium.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from time import sleep
+import unittest
+
+class TestRegistration(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def testinvalidEmail(self):
+        pass
